a699fcf6563d5e5232fbbdacca04b4a467d948f1
AlwaysBlind <ulfbaeltdjur@gmail.com>
2020-09-19 21:05:25 +0200

    Updates

---
+++ precache-manifest.787ec12940a30f4152ace7475c5fce2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77edc5b830b466d3ea579f75e6ad982b",
+    "url": "/II-frontend/index.html"
+  },
+  {
+    "revision": "59a36b0a9e00bdaa8aca",
+    "url": "/II-frontend/static/css/main.4e46c695.chunk.css"
+  },
+  {
+    "revision": "69c81f6713c2ad09006e",
+    "url": "/II-frontend/static/js/2.2697ff17.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/II-frontend/static/js/2.2697ff17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a36b0a9e00bdaa8aca",
+    "url": "/II-frontend/static/js/main.34c8731f.chunk.js"
+  },
+  {
+    "revision": "7aeb34d7af35cc3c0862",
+    "url": "/II-frontend/static/js/runtime-main.5b6a956f.js"
+  }
+]);

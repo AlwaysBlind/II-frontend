d4a1414b77f13cdbddd2cf8ebf34e9811c2e6981
AlwaysBlind <ulfbaeltdjur@gmail.com>
2020-12-03 14:26:18 +0100

    Fix layout

--- src/components/NavbarHeader.js
+++ src/components/NavbarHeader.js
@@ -4,7 +4,7 @@ const { Navbar } = require("react-bootstrap");
 const NavbarHeader = () => {
     return (
         <Navbar bg="secondary">
-            <Navbar.Brand href="/II-frontend">
+            <Navbar.Brand href="/II-frontend/">
                 <i
                     className="d-inline-block align-top fas fa-battery-full fa-lg"
                     alt="React Bootstrap logo"
